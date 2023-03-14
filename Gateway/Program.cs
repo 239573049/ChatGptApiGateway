750c51c34026bd74216a3c2ff1e2749b73fb141c
token <239573049@qq.com>
2023-03-14 15:51:58 +0800

    修复无法使用

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,10 +1,45 @@
+using System.Net.Http.Headers;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+var message = new HttpClientHandler();
+message.ServerCertificateCustomValidationCallback += (requestMessage, certificate2, arg3, arg4) => true;
+
+builder.Services.AddTransient((servicer) => new HttpClient(message));
 
 var app = builder.Build();
 
-app.MapReverseProxy();
+
+app.Run(async (context) =>
+{
+    var http = context.RequestServices.GetService<HttpClient>();
+
+    http.DefaultRequestHeaders.Remove("Authorization");
+    http.DefaultRequestHeaders.Add("Authorization", context.Request.Headers.Authorization.ToString());
+    var stream = new MemoryStream();
+    await context.Request.Body.CopyToAsync(stream);
+
+    var json = Encoding.UTF8.GetString(stream.ToArray());
+    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+    var data = await http.PostAsync("https://api.openai.com"+ context.Request.Path + context.Request.QueryString.Value, content);
+
+    foreach (var item in data.Headers)
+    {
+        if (item.Key.ToLower() == "content-type")
+        {
+            continue;
+        }
+        if (context.Response.Headers.ContainsKey(item.Key))
+        {
+            context.Response.Headers.Remove(item.Key);
+        }
+        context.Response.Headers.Add(item.Key, item.Value.ToString());
+    }
+
+    await data.Content.CopyToAsync(context.Response.Body);
+
+});
 
 await app.RunAsync();
