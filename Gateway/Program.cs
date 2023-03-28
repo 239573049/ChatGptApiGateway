6f658734eb98969a795e2f41624d4c5354e4087a
token <239573049@qq.com>
2023-03-28 19:38:26 +0800

    更新使用yarp代理
    Signed-off-by: token <239573049@qq.com>

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,79 +1,19 @@
-using System.Diagnostics;
-using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped((servier) =>
-{
-    var message = new HttpClientHandler();
-    message.ServerCertificateCustomValidationCallback += (_, _, _, _) => true;
-    return new HttpClient(message);
-});
-
-// 允许所有策略Cors
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", corsBuilder =>
-    {
-        corsBuilder.SetIsOriginAllowed((string _) => true).AllowAnyMethod().AllowAnyHeader()
-            .AllowCredentials();
-    });
-});
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-
-app.UseCors("CorsPolicy");
-
-app.Run(async (context) =>
+app.Use(async (context,next) =>
 {
-    var logger = context.RequestServices.GetService<ILogger>();
-    var stopwatch = Stopwatch.StartNew();
-    logger?.LogWarning("start Request:{RequestPath} ", context.Request.Path);
-    try
-    {
-        var http = context.RequestServices.GetRequiredService<HttpClient>();
-        http.DefaultRequestHeaders.Remove("Authorization");
-        var token = context.Request.Headers.FirstOrDefault(x => x.Key == "Authorization");
-        if (!string.IsNullOrEmpty(token.Value))
-        {
-            http.DefaultRequestHeaders.Add("Authorization", token.Value.ToString());
-        }
-
-        var stream = new MemoryStream();
-        await context.Request.Body.CopyToAsync(stream);
-        var json = Encoding.UTF8.GetString(stream.ToArray());
-        var content = new StringContent(json, Encoding.UTF8, "application/json");
-        var data = await http.PostAsync(
-            "https://api.openai.com" + context.Request.Path + context.Request.QueryString.Value, content);
-
-        foreach (var item in data.Headers)
-        {
-            if (item.Key.ToLower() == "content-type")
-            {
-                continue;
-            }
-
-            if (context.Response.Headers.ContainsKey(item.Key))
-            {
-                context.Response.Headers.Remove(item.Key);
-            }
-
-            context.Response.Headers.Add(item.Key, item.Value.ToString());
-        }
-
-        await data.Content.CopyToAsync(context.Response.Body);
-    }
-    catch (Exception e)
-    {
-        logger?.LogError(e, "error server request:{Path} elapsed time {ElapsedMilliseconds} ms", context.Request.Path,
-            stopwatch.ElapsedMilliseconds);
-        throw;
-    }
-
-    logger?.LogWarning("stop request:{Path} elapsed time {ElapsedMilliseconds} ms", context.Request.Path,
-        stopwatch.ElapsedMilliseconds);
+    var logger = context.RequestServices.GetService<ILogger<Program>>();
+    var ip = context.Connection.RemoteIpAddress?.MapToIPv4().ToString();
+    logger.LogInformation("IP: {ip} 请求时间：{Date}", ip,DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+    await next.Invoke(context);
 });
 
+
+app.MapReverseProxy();
 
 await app.RunAsync();
